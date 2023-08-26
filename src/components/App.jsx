d44f2ab337f5c64f0f3399d972781662cb14f7b6
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-08-26 19:27:04 +0300

    clone hw 03

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,82 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { GlobalStyle } from './GlobalStyle';
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Section } from './Section/Section';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const savedLocalStorage = localStorage.getItem('contact');
+    if (savedLocalStorage !== null) {
+      this.setState({
+        contacts: JSON.parse(savedLocalStorage),
+      });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contact', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = newName => {
+    if (
+      this.state.contacts.map(contact => contact.name).includes(newName.name)
+    ) {
+      alert(`${newName.name} is already in contacts`);
+    } else {
+      this.setState(prevState => {
+        return {
+          contacts: [...prevState.contacts, newName],
+        };
+      });
+    }
+  };
+
+  changeFilter = newFilter => {
+    this.setState({
+      filter: newFilter,
+    });
+  };
+
+  handleDelete = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    const visibleContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <div>
+        <GlobalStyle />
+
+        <Section title="Phonebook">
+          <ContactForm addContact={this.addContact} />
+        </Section>
+
+        <Section title="Contacts">
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactList
+            contacts={visibleContacts}
+            onDelete={this.handleDelete}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
